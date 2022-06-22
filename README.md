4cd1e91f75aad22eebee30ff940d4b6c9415629f
Aline Hornoff <australine@gmail.com>
2022-06-22 16:43:32 +0800

    Code for app.js

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# JavaScript_Challenge
+# JavaScript_Challenge - Aliens R Real
+
+Aliens are real! In this bootcamp challengeeye-witness reports of extra-terrestial menace need to shared with the world via online platform so the matter of 'Are aliens real?' can finally be put to rest.
+
+The problem... The collection is to large to search through manually. 
+THis challenge consits of 2 parts - Level 1 and Level 2
+
+### The following technologies will be used to summary the information online
+* JavaScript
+* HTML
+* CSS
+* D3.js
+
+### Level 1 - Automatic Table and Data Search:
+* Create a basic HTML web page or use the index.html file provided (we recommend building your own custom page!).
+* Using the UFO dataset provided in the form of an array of JavaScript objects, write code that appends a table to your web page and then adds new rows of data for each UFO sighting.
+* Make sure you have a column for date/time, city, state, country, shape, and comment at the very least.
+* Use a date form in your HTML document and write JavaScript code that will listen for events and search through the date/time column to find rows that match user input.
+
+### Level 2 - Multiple Search Categories
+* Complete all of Level 1 criteria.
+* Using multiple input tags and/or select dropdowns, write JavaScript code so the user can to set multiple filters and search for UFO sightings using the following criteria based on the table columns:
+
+1. date/time
+2. city
+3. state
+4. country
+5. shape
+
+### Contents
+UFO_Level_1 - contains HTML, css, images and js
+UFO_Level_2 - contains
---
+++ UFO_Level_1/StarterCode/static/js/app.js
@@ -0,0 +1,69 @@
+// from data.js
+var tableData = data;
+
+// YOUR CODE HERE!
+
+// Create reference to table data
+var UFO_table = d3.select("tbody");
+
+// Set UFO Sighting values fore ach column 
+tableData.forEach(function(UFO_sighting) {
+    // Check values
+    console.log(UFO_sighting);
+
+    // For each table row 'tr' append one UFO sighting value
+    var row = UFO_table.append("tr");
+
+    // Utilise 'Object.entries' to console.log each UFO sighting value
+    Object.entries(UFO_sighting).forEach(function([key, value]) {
+        // Check values
+        console.log(key, value);
+
+        // Append a cell to the row for each value
+        var Cell = row.append("td");
+        Cell.text(value);
+    });
+});
+
+// Select the button
+var Button = d3.select("filter-btn");
+Button.on("Click", function() {
+
+    UFO_table.html("");
+
+    // Select input date
+    var InputElement = d3.select("#datetime");
+
+    // Get value property of date
+    var InputValue = InputElement.property("value");
+
+    // Console.log input value
+    console.log(InputValue);
+
+    // Filter data with datetime equal to input value
+    var FilteredData = tableData.filter(Sighting => Sighting.datetime === InputValue);
+
+    // Console.log filter values
+    console.log(FilteredData);
+
+    FilteredData.forEach(function(Selections) {
+        // Console.log Selections
+        console.log(Selections);
+
+        // Append one table row 'tr' for each UFO Sighting Object
+        var row = UFO_table.append("tr");
+
+        // Utilise 'Object.entries' to console.log each UFO sighting value
+        Object.entries(Selections).forEach(function([key, value]) {
+            // Console.log key and value
+            console.log(key, value);
+
+            // Append a cell to the row for ach value
+            var Cell = row.append("td");
+            Cell.text(value);
+
+        });
+    });
+});
+
+    
